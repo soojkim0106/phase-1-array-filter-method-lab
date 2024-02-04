7f059313a85ddadcd7890b6446bba937ff2f0e7b
Soojungkim <soojung.kimm@gmail.com>
2024-02-04 15:15:29 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(name, string){
+    const driver = name.filter(driverName => driverName.toLowerCase() === string.toLowerCase())
+
+    return driver
+}
+
+function fuzzyMatch(name, string){
+    const driver = name.filter(driverName => driverName.slice(0,2) === string)
+
+    return driver
+}
+
+function matchName(driver, string){
+    const drivers = driver.filter(driverObject => driverObject.name === string)
+
+    return drivers
+}
